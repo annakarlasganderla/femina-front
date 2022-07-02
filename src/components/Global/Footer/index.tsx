fecd205e431df02a6a4239621dd8e2e0bd15303a
VitorDanielAdams <vitor.adams@outlook.com>
2022-07-02 17:09:09 -0300

    feat - Component Footer Estilizado - *Erro ao importar logo

--- src/components/Global/Footer/index.tsx
+++ src/components/Global/Footer/index.tsx
@@ -1,21 +1,38 @@
-import * as S from "./styles"
+import * as S from "./styles";
+import { AiOutlineInstagram, AiOutlineWhatsApp, AiFillFacebook } from "react-icons/ai";
+import { ST } from "next/dist/shared/lib/utils";
+
 export const Footer = () => {
     return (
     <footer>
         <S.FooterContainer>
-            <div className="Endereço"> 
-                <div>
-                    <h2>Endereço</h2>
-                </div>
-            <div className="Imagem">  
-                <div>
-                    <img src="logo.jpg" alt="Logo.Footer" />
-                </div>
-                </div>
-            <div className="Contato">
-                    <h2>Entre em contato conosco</h2>
-                </div>
-            </div>
+            <S.InfoArea> 
+                <S.TextTitle>Endereço</S.TextTitle>
+                <S.TextContent>
+                    <S.TextParagraph>Rua Rui Barbosa, n°2050</S.TextParagraph>
+                    <S.TextParagraph>Centro, Itaipulândia -Pr</S.TextParagraph>
+                    <S.TextParagraph>CEP: 85880-000</S.TextParagraph>
+                </S.TextContent>
+            </S.InfoArea>  
+            <S.ImageArea src="../../../public/logo.png" alt="Logo" />
+            <S.InfoArea>
+                <S.TextTitle>Entre em contato conosco</S.TextTitle>
+                <S.TextContent>
+                    <S.ContactArea>
+                        <AiOutlineInstagram color="white" size="10px" />
+                        <S.TextParagraph>@femina_modaintima</S.TextParagraph>
+                    </S.ContactArea>
+                    <S.ContactArea>
+                        <AiOutlineWhatsApp color="white" size="10px" />
+                        <S.TextParagraph>(45) 9 9984-6175</S.TextParagraph>
+                    </S.ContactArea>
+                    <S.ContactArea>
+                        <AiFillFacebook color="white" size="10px" />
+                        <S.TextParagraph>facebook/femina_modaintima</S.TextParagraph>
+                    </S.ContactArea>
+                </S.TextContent>
+            </S.InfoArea>
+            
         </S.FooterContainer>
         
     </footer>
